0a62b1807d5202302307bf99487321404b947141
Martin <martinknoetze@gmail.com>
2023-03-31 09:45:29 -0600

    Cleaned up the code

--- app.cc
+++ app.cc
@@ -17,7 +17,7 @@
 #define RECORD_LENGTH 20
 #define MAX_NEIGHBOURS 26
 
-typedef enum {DISCOVERY_REQUEST, DISCOVERY_RESPONSE, CREATE_RECORD, DELETE_RECORD, RETRIEVE_RECORD, RESPONSE, IDLE = -1} protocol;
+typedef enum {DISCOVERY_REQUEST, DISCOVERY_RESPONSE, CREATE_RECORD, DELETE_RECORD, RETRIEVE_RECORD, RESPONSE} protocol;
 
 typedef struct {
     short senderGroupId;
@@ -36,6 +36,11 @@ typedef struct {
     char recordMessage[RECORD_LENGTH];
 } record;
 
+typedef struct {
+    char messageSize;
+    char * messageContent;
+} messageDetails;
+
 record database[MAX_RECORDS];
 
 char neighbours[MAX_NEIGHBOURS];
@@ -48,47 +53,46 @@ int neighbourCount;
 
 char currentRequestNumber;
 
-protocol currentProtocol;
-
-message * messagePtr;
-message * receiveMessagePtr;
-message * discoveryResponceMessagePtr
-
 int sfd = -1;
 
 /**
- * Assembles the message in the message struct into a character array
+ * Creates a struct that holds the message content and size of the message
  * 
  * Parameters:
  *      messagePtr: a pointer to a message struct
  * 
  * Return:
- *      Returns a character array that holds the required message to be sent
+ *      a struct with the message contents and size
 */
-char * assembleMessage(message * messagePtr) {
+messageDetails getMessageDetials(message * messagePtr) {
+    messageDetails details;
+    
     char * p;
-
+    char size;
     switch(messagePtr->messageType) 
     {
     case DISCOVERY_REQUEST:
     case DISCOVERY_RESPONSE:
-        p = (char*)umalloc(6);
+        size = 6;
+        p = (char*)umalloc(size);
         break;
 
     case RETRIEVE_RECORD:
     case DELETE_RECORD:
-        p = (char*)umalloc(8);
-
+        size = 8;
+        p = (char*)umalloc(size);
         p[6] = messagePtr->recordIndex;
         p[7] = 0x00;
         break;
     
     case CREATE_RECORD:
-        p = (char*)umalloc(26);
+        size = 26;
+        p = (char*)umalloc(size);
         strcpy(p + 6, messagePtr->messageRecord);
         break;
     
     case RESPONSE:
+        size = 28;
         p = (char*)umalloc(28);
         p[6] = messagePtr->status;
         p[7] = 0x00;
@@ -102,57 +106,27 @@ char * assembleMessage(message * messagePtr) {
     p[4] = messagePtr->senderId;
     p[5] = messagePtr->destinationId;
 
-    return p;
-}
-
-/**
- * Determines the size of the packet to send based on the message type
- * 
- * Parameter:
- *      messageType: a character representing the message type
- * 
- * Return:
- *      an integer of how big the packet needs to be
-*/
-int getPacketSize(char messageType) {
-    int size = 0;
+    details.messageSize = size + 4;
+    details.messageContent = p;
     
-    switch(messageType) 
-    {
-    case DISCOVERY_REQUEST:
-    case DISCOVERY_RESPONSE:
-        size = 6;
-        break;
-
-    case RETRIEVE_RECORD:
-    case DELETE_RECORD:
-        size = 8;
-        break;
-    
-    case CREATE_RECORD:
-        size = 26;
-        break;
-    
-    case RESPONSE:
-        size = 28;
-        break;
-    }
-
-    return size + 4; // add four bytes for the two bytes before and after the payload
+    return details;
 }
 
 /**
  * State machine for handling transmitting messages
 */
 fsm transmitter (message * messagePtr) {
+
     state Transmit_Message:
 
-        char * assembledMessage = assembleMessage(messagePtr);
+        messageDetails details = getMessageDetials(messagePtr);
+
+        char * assembledMessage = details.messageContent;
 
         address spkt;
 
         // populate packet pointer
-        spkt = tcv_wnp (Transmit_Message, sfd,  getPacketSize(messagePtr->messageType));
+        spkt = tcv_wnp (Transmit_Message, sfd,  details.messageSize);
         spkt [0] = 0;
         char * p = (char*)(spkt + 1); // skip first 2 bytes
         strcpy(p, assembledMessage);
@@ -161,6 +135,184 @@ fsm transmitter (message * messagePtr) {
 
     state Confirm_Transmission:
         finish;
+}
+
+message * initialize_response(message * receiveMessagePtr){
+    
+    message *response = (message *)umalloc(sizeof(message));
+
+	response->senderGroupId = groupId;
+	response->messageType = RESPONSE;
+	response->requestNumber = receiveMessagePtr->requestNumber;
+	response->senderId = nodeId;
+	response->destinationId = receiveMessagePtr->senderId;
+	
+    strcpy(response->messageRecord,"\0");
+}
+
+message *add_entry(message *receiveMessagePtr){
+	message *response = initialize_response(receiveMessagePtr);
+
+	if(recordCount >= MAX_RECORDS){
+		response->status = 0x02;
+		return response;
+	}
+
+    database[recordCount].ownerId = receiveMessagePtr->senderId;
+    database[recordCount].timeStamp = time(NULL);
+    strcpy(database[recordCount].recordMessage, receiveMessagePtr->messageRecord);
+	
+    recordCount++;
+	
+    response->status = 0x01;
+	
+    return response;
+}
+
+message *delete_entry(message *receiveMessagePtr){
+	message *response = initialize_response(receiveMessagePtr);
+	
+    if(recordCount == 0){
+		response->status = 0x03;
+		return response;
+	}
+
+	if(receiveMessagePtr->recordIndex >= recordCount){ //Nothing here
+		response->status = 0x04;
+		return response;
+	}
+	
+    response->status = 0x01;
+	
+    //delete the element
+	database[receiveMessagePtr->recordIndex] = database[recordCount-1];
+	recordCount--;
+
+	return response;
+}
+
+message *retrieve_entry(message *receiveMessagePtr){
+	message *response = initialize_response(receiveMessagePtr);
+	
+    if(recordCount == 0){
+		response->status = 0x03;
+		return response;
+	}
+	
+    if(receiveMessagePtr->recordIndex >= recordCount){ //Nothing here
+		response->status = 0x04;
+		return response;
+	}
+	
+    response->status = 0x01;
+	
+    strcpy(response->messageRecord, database[receiveMessagePtr->recordIndex].recordMessage);
+	
+    return response;
+}
+
+/**
+ * This FSM will run and listen to all incoming packets and sort them as required
+ */
+fsm receiver {
+    
+    address rpkt;
+
+    char * payload;
+
+    message * receiveMessagePtr;
+
+    state Initialize_Receiver:
+        receiveMessagePtr = (message *) umalloc(sizeof(message));
+
+    state Receiving:
+
+        // wait for a message
+        rpkt = tcv_rnp (Receiving, sfd);
+
+        // start parcing the payload
+        // skip first 2 bytes and cast to char pointer
+        payload = (char *) (rpkt+1);
+
+        // parce the payload
+        receiveMessagePtr -> senderGroupId = (short) *payload;
+        receiveMessagePtr -> messageType = *(payload+2);
+        receiveMessagePtr -> requestNumber = *(payload+3);
+        receiveMessagePtr -> senderId = *(payload+4);
+        receiveMessagePtr -> destinationId = *(payload+5);
+
+        // checks to see if recived packet belongs to the Group, if not returns to state finish 
+        if (receiveMessagePtr -> senderGroupId != groupId){
+            proceed Finish;
+        }
+
+    state Processing:
+
+        message * response;
+        message discoveryResponseMessage;
+
+        // switches to the responce type
+        switch (receiveMessagePtr -> messageType) {
+            
+        case DISCOVERY_REQUEST:
+            
+            discoveryResponseMessage.senderGroupId = groupId;
+            discoveryResponseMessage.messageType = DISCOVERY_RESPONSE;
+            discoveryResponseMessage.requestNumber = receiveMessagePtr -> requestNumber;
+            discoveryResponseMessage.senderId = nodeId;
+            discoveryResponseMessage.destinationId = receiveMessagePtr -> senderId;
+            call transmitter(&discoveryResponseMessage, Finish);
+            break;
+
+        case CREATE_RECORD:
+            // proceeds to finish if this this node was't the intended recipient
+            if (receiveMessagePtr -> destinationId != nodeId){
+                proceed Finish;
+            }
+
+            strcpy(receiveMessagePtr -> messageRecord, payload+6);
+
+	        call transmitter(add_entry(receiveMessagePtr), Finish);
+            break;
+        
+        case DELETE_RECORD:
+            if (receiveMessagePtr -> destinationId != nodeId) {
+                proceed Finish;
+            }
+
+            receiveMessagePtr -> recordIndex = *(payload+6);
+	        //response = delete_entry();
+	        call transmitter(delete_entry(receiveMessagePtr), Finish);
+            break;
+        
+        case RETRIEVE_RECORD:
+            if (receiveMessagePtr -> destinationId != nodeId){
+                proceed Finish;
+            }
+
+            receiveMessagePtr -> recordIndex = *(payload+6);
+	        //response = retrieve_entry();
+	        call transmitter(retrieve_entry(receiveMessagePtr), Finish);
+            break;
+
+        case RESPONSE:
+            // filter out all packets that are not part of the currentRequestNumber
+            if (receiveMessagePtr -> requestNumber != currentRequestNumber){
+                proceed Finish;
+            }
+
+            receiveMessagePtr -> status = *(payload+6);
+            strcpy(receiveMessagePtr -> messageRecord, payload+8);
+
+            currentRequestNumber = 0;
+            break;
+        }
+        
+    state Finish:
+        // clear memory and return back to reciving
+        tcv_endp (rpkt); // I think this frees the memory
+        proceed Receiving;
+
 }
 
 /**
@@ -191,23 +343,25 @@ fsm find {
     int sendCount;
     int i;
 
+    message findMessage;
+
     state Initialize:
         i = 0;
         sendCount = 0;
         currentRequestNumber = randomNumber();
 
-        messagePtr -> senderGroupId = groupId;
-        messagePtr -> messageType = 0;
-        messagePtr -> requestNumber = currentRequestNumber;
-        messagePtr -> senderId = nodeId;
-        messagePtr -> destinationId = 0;
+        findMessage.senderGroupId = groupId;
+        findMessage.messageType = 0;
+        findMessage.requestNumber = currentRequestNumber;
+        findMessage.senderId = nodeId;
+        findMessage.destinationId = 0;
 
     state Send_Discovery_Request:
         if(sendCount == 2) {
             proceed Display_Neighbours;
         }
 
-        call transmitter(messagePtr, Wait);
+        call transmitter(&findMessage, Wait);
         sendCount++;
 
     state Wait:
@@ -261,6 +415,10 @@ fsm root {
 
     char receiverId;
 
+    message * messagePtr;
+
+    protocol currentProtocol;
+
     state Initialize:
         recordCount = 0;
 
@@ -268,8 +426,6 @@ fsm root {
         groupId = 1;
 
         messagePtr = (message *) umalloc(sizeof(message));
-        receiveMessagePtr = (message *) umalloc(sizeof(message));
-        discoveryResponceMessagePtr = (message *) umalloc(sizeof(message));
         
         // Set up cc1350 board
         phys_cc1350(0, MAX_PACKET_LENGTH);
@@ -287,10 +443,9 @@ fsm root {
 			halt();
 		}
         // Start receiver
-        runfsm reciever;
+        runfsm receiver;
 
     state Menu_Header:
-        currentProtocol = IDLE;
         ser_outf(Menu_Header, "\r\nGroup %d Device #%d (%d/%d records)\r\n", groupId, nodeId, recordCount, MAX_RECORDS);
     
     state Options:
@@ -371,9 +526,6 @@ fsm root {
 // - - - - - - - - - - - - Find Neighbour - - - - - - - - - - - - - //
     state Find_Neighbours:
         call find(Menu_Header);
-        // currentProtocol = DISCOVERY_REQUEST;
-        // receiverId = 0;
-        // proceed Transmit_Message;
 
 // - - - - - - - - - - - - Create Recrods - - - - - - - - - - - - - //    
     state Create_Record:
@@ -430,157 +582,13 @@ fsm root {
 
 // - - - - - - Assemble Common Parts and Send Message - - - - - - - //
     state Transmit_Message:
+        currentRequestNumber = randomNumber();
+
         messagePtr -> senderGroupId = groupId;
         messagePtr -> messageType = currentProtocol;
-        messagePtr -> requestNumber = randomNumber();
+        messagePtr -> requestNumber = currentRequestNumber;
         messagePtr -> senderId = nodeId;
         messagePtr -> destinationId = receiverId;
 
         call transmitter(messagePtr, Menu_Header);
-}
-
-void initialize_response(message *response){
-	response->senderGroupId = groupId;
-	response->messageType = RESPONSE;
-	response->requestNumber = receiveMessagePtr->requestNumber;
-	response->senderId = nodeId;
-	response->destinationId = receiveMessagePtr->senderId;
-	strcpy(response->messageRecord,"\0");
-}
-
-message *add_entry(){
-	message *response = umalloc(sizeof(message));
-	initialize_response(response);
-	if(recordCount >= MAX_RECORDS){
-		response->status = 0x02;
-		return response;
-	}
-	record *newRecord = umalloc(sizeof(record));
-	newRecord->timeStamp = time(NULL);
-	newRecord->ownerId = receiveMessagePtr->senderId;
-	strcpy(newRecord->recordMessage, receiveMessagePtr->messageRecord);
-	database[recordCount] = *newRecord;
-	recordCount++;
-	response->status = 0x01;
-	return response;
-}
-
-message *delete_entry(){
-	message *response = umalloc(sizeof(message));
-	initialize_response(response);
-	if(recordCount == 0){
-		response->status = 0x03;
-		return response;
-	}
-	if(receiveMessagePtr->recordIndex >= recordCount){ //Nothing here
-		response->status = 0x04;
-		return response;
-	}
-	response->status = 0x01;
-	//delete the element
-	database[receiveMessagePtr->recordIndex] = database[recordCount-1];
-	recordCount--;
-	return response;
-}
-
-message *retrieve_entry(){
-	message *response = umalloc(sizeof(message));
-	initialize_response(response);
-	if(recordCount == 0){
-		response->status = 0x03;
-		return response;
-	}
-	if(receiveMessagePtr->recordIndex >= recordCount){ //Nothing here
-		response->status = 0x04;
-		return response;
-	}
-	response->status = 0x01;
-	strcpy(response->messageRecord, database[receiveMessagePtr->recordIndex].recordMessage);
-	return response;
-
-/**
- * This FSM will run and listen to all incoming packets and sort them as rwquired
- * 
- * 
- */
-fsm receiver {
-
-    address rpkt;
-
-    state Receiving:
-        // wait for a message
-        rpkt = tcv_rnp (Receiving, sfd);
-
-        // start parcing the payload
-        // skip first 2 bytes and cast to char pointer
-        char * payload = (char *) (rpkt+1);
-        
-        // parce the payload
-        receiveMessagePtr -> senderGroupId = (short) *payload;
-        receiveMessagePtr -> messageType = *(payload+2);
-        receiveMessagePtr -> requestNumber = *(payload+3);
-        receiveMessagePtr -> senderId = *(payload+4);
-        receiveMessagePtr -> destinationId = *(payload+5);
-
-        // checks to see if recived packet belongs to the Group, if not returns to state finish 
-        if (receiveMessagePtr -> senderGroupId != groupId)
-            proceed finish;
-
-	message *response;
-        // switches to the responce type
-        switch (receiveMessagePtr -> messageType)
-        {
-            case DISCOVERY_REQUEST:
-            discoveryResponceMessagePtr -> senderGroupId = groupId;
-            discoveryResponceMessagePtr -> messageType = DISCOVERY_RESPONSE;
-            discoveryResponceMessagePtr -> requestNumber = receiveMessagePtr -> requestNumber;
-            discoveryResponceMessagePtr -> senderId = nodeId;
-            discoveryResponceMessagePtr -> destinationId = receiveMessagePtr -> senderId;
-            call transmitter(discoveryResponceMessagePtr, Wait);
-            break;
-
-
-        case CREATE_RECORD:
-            // proceeds to finish if this this node was't the intended recipient
-            if (receiveMessagePtr -> destinationId != nodeId)
-                proceed finish;
-            strcpy(receiveMessagePtr -> messageRecord, *(payload+6));
-	    response = add_entry();
-	    call transmitter(response, finish);
-            break;
-        
-        case DELETE_RECORD:
-            if (receiveMessagePtr -> destinationId != nodeId)
-                proceed finish;
-            receiveMessagePtr -> recordIndex = *(payload+6);
-	    response = delete_entry();
-	    call transmitter(response, finish);
-            break;
-        
-        case RETRIEVE_RECORD:
-            if (receiveMessagePtr -> destinationId != nodeId)
-                proceed finish;
-            receiveMessagePtr -> recordIndex = *(payload+6);
-	    response = retrieve_entry();
-	    call transmitter(response, finish);
-            break;
-
-        case RESPONSE:
-            // filter out all packets that are not part of the currentRequestNumber
-            ( if discoveryResponceMessagePtr -> requestNumber != currentRequestNumber)
-                proceed finish;
-
-            receiveMessagePtr -> status = *(payload+6);
-            strcpy(receiveMessagePtr -> messageRecord, *(payload+8));
-            break;
-        }
-    state finish
-        // clear memory and return back to reciving
-        tcv_endp (rpkt); // I think this frees the memory
-        proceed Receiving;
-
-
-
-
-
 }
